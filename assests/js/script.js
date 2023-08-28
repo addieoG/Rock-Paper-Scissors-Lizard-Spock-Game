7c14ff1fdb82617752111fa0e781bcf88152b84f
aidan mulvaney <65428883+addieoG@users.noreply.github.com>
2023-08-28 14:50:01 +0100

    Feat
    Add script for reset button and script for best of three button

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,22 +1,23 @@
+
+
 const playerChoice = document.querySelector("#players-choice");
 const computersChoice = document.querySelector("#computers-choice");
 const playerSelection = document.querySelectorAll(".player-selection");
 const displayResult = document.querySelector("#display-result");
 const playerScoreElement = document.querySelector("#player-score");
 const computerScoreElement = document.querySelector("#computer-score");
+
 let player;
 let computer;
 let result;
 let playerScore = 0;
 let computerScore = 0;
-runGame()
-
-
 
 /**
  * Displays the players selection, computers selection and score board
  */
-function runGame() {
+function bestOfThree() {
+    alert("hello");
     playerSelection.forEach(button => button.addEventListener("click", () => {
         player = button.textContent;
         computerSelection();
@@ -26,9 +27,8 @@ function runGame() {
         playerScoreElement.textContent = playerScore;
         computerScoreElement.textContent = computerScore;
         scoreBoard();
-
-
-
+        if (playerScore == 2)
+            alert("Game Over");
     }));
 };
 /**
@@ -56,14 +56,30 @@ function computerSelection() {
     }
 };
 
-/** logic statement to determine winner */
+/** 
+ * logic statement that displays if you won, loss or it's a draw to the dom
+*/
 function determineWinner() {
-    if (player == computer) {
-        return "Result!! draw!"
-    } else {
-        return "Broken code"
+    if ((player == "Rock" && (computer == "Scissors" || computer == "Lizard")) ||
+        (player == "Paper" && (computer == "Rock" || computer == "Spock")) ||
+        (player == "Scissors" && (computer == "Paper" || computer == "Lizard")) ||
+        (player == "Lizard" && (computer == "Paper" || computer == "Spock")) ||
+        (player == "Spock" && (computer == "Rock" || computer == "Scissors"))) {
+        return "You Win"
+    } else if ((player == "Rock" && (computer == "Scissors" || computer == "Lizard")) ||
+        (computer == "Paper" && (player == "Rock" || player == "Spock")) ||
+        (computer == "Scissors" && (player == "Paper" || player == "Lizard")) ||
+        (computer == "Lizard" && (player == "Paper" || player == "Spock")) ||
+        (computer == "Spock" && (player == "Rock" || player == "Scissors"))) {
+        return "Sorry you lose!"
+    } else if (computer == player) {
+        return "it's a tie"
     }
 };
+
+/** 
+ * logic statement that determines if the player or the computer has won
+*/
 
 function scoreBoard() {
     if ((player == "Rock" && (computer == "Scissors" || computer == "Lizard")) ||
@@ -80,5 +96,14 @@ function scoreBoard() {
         (computer == "Spock" && (player == "Rock" || player == "Scissors"))) {
         computerScore++;
         computerScoreElement.textContent = playerScore;
+    } else if (computer == player) {
+        //do nothing 
     }
+}
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
 }
